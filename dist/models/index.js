8c9e79c60494284cf223b3274e5da586062a918c
alowave <49539029+Lewui@users.noreply.github.com>
2020-03-25 17:24:07 +0200

    s
    s

--- dist/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Guilds = require("./guild");
-const Profiles = require("./profiles");
-const Notes = require("./notes");
-Notes.sync();
-Profiles.sync();
-Guilds.sync();
