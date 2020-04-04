0ac135ee48af0e1f1a258a7fdfbe6b918eac945c
alowave <49539029+Lewui@users.noreply.github.com>
2020-04-04 19:06:06 +0300

    Create createpassport.ts

---
+++ src/commands/fun/createpassport.ts
@@ -0,0 +1,20 @@
+import { Command } from "../../domain/Command";
+import { Message } from "discord.js";
+
+class CreatePass extends Command {
+    constructor(){
+        super({
+            name: "createpass",
+            description: "Введите данную команду что бы получить бонус в виде монет! Можно получить только раз в 12 часов.",
+            aliases: [""],
+            category: "fun",
+            usage: ">daily"
+        });
+    }
+
+    async run(message: Message, args: Array<string>) {
+
+    }
+}
+
+export = CreatePass;
